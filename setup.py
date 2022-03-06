52d58b40ec6541ff3e286d1c80d3117b48320160
TheoBuchwald <theo.buchmail@gmail.com>
2022-03-06 01:40:16 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 
 setup(
     name='KurtGroup',
-    version='1.0.1',
+    version='1.1.0',
     author='Theo Juncker von Buchwald',
     author_email='fnc970@alumni.ku.dk',
     packages=['Kurt'],
+    package_data={'Kurt': ['*.py', '*.json']},
+    # include_package_data=True,
     license='LICENCSE',
     description='A package containing scripts and programs developed and used in Professor Kurt V. Mikkelsens group at the University of Copenhagen.',
     long_description=open('README.md').read(),
-    install_requires=['numpy >= 1.13.0', 'requests >= 2.4', 'ase >= 3.19.0', 'matplotlib'],
+    install_requires=['numpy >= 1.13.0', 'requests >= 2.4', 'ase >= 3.19.0', 'matplotlib', 'json'],
     python_requires='>=3.6'
 )
